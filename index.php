f857629e53035ff2ac24fef4d16b8e997d34e150
mohankrishnahb@gmail.com <mohankrishnahb@gmail.com>
2022-10-29 15:11:41 +0530

    added connection config

--- index.php
+++ index.php
@@ -11,5 +11,23 @@
 <?php /*include_once("index.html");*/
 echo("<h1>Hi</h1>")
 ?>
+<?php
+$servername = "sql12.freemysqlhosting.net"; //localhost
+$username = "sql12530105"; //root
+$password = "RJRTlL9Qr4"; //root
+$database = "sql12530105"; //cpntact
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+<!-- dfgdf -->
+
 </body>
 </html>
