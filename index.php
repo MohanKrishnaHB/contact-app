6a23184fdac1005bcbe53cf09e19143dba7863a4
mohankrishnahb@gmail.com <mohankrishnahb@gmail.com>
2022-10-29 15:50:56 +0530

    form

--- index.php
+++ index.php
@@ -16,10 +16,8 @@ $servername = "sql12.freemysqlhosting.net"; //localhost
 $username = "sql12530105"; //root
 $password = "RJRTlL9Qr4"; //root
 $database = "sql12530105"; //cpntact
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
-
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
---
+++ insert.php
@@ -0,0 +1,10 @@
+<?php
+
+$id= $_POST['id']
+$name= $_POST['name']
+$email= $_POST['email']
+$phno= $_POST['phoneNumber']
+
+echo($name)
+
+?>
