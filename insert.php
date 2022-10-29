026004e8d67d9d8cb5dcf1af0e7f8462f3cf245c
mohankrishnahb@gmail.com <mohankrishnahb@gmail.com>
2022-10-29 16:00:08 +0530

    insert

--- insert.php
+++ insert.php
@@ -1,10 +1,30 @@
 <?php
+$servername = "sql12.freemysqlhosting.net"; //localhost
+$username = "sql12530105"; //root
+$password = "RJRTlL9Qr4"; //root
+$database = "sql12530105"; //cpntact
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 
 $id= $_POST['id'];
 $name= $_POST['name'];
 $email= $_POST['email'];
 $phno= $_POST['phoneNumber'];
 
-echo("$name");
+$sql = "INSERT INTO contact (id, name, email, phoneNumber)VALUES ($id, $name, $email, $phno)";
 
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
+
+
